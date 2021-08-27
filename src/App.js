108ab7debdbf142a15643f29114d34b82e93ad8f
hakankaraahmet <karaahmethkn@gmail.com>
2021-08-27 14:18:44 +0300

    modified files with small features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import { InitialState } from "./store/InitialState";
+import TaskList from "./components/TaskList";
+import CreateTask from "./components/CreateTask";
+import Footer from "./components/Footer"
 
-function App() {
+const App = () => {
+  const [tasks, setTask] = useState(InitialState);
+  const [isShow, setIsShow] = useState(true);
+
+  //ADD TASK
+  const addTask = (task) => {
+    const id = Date.now();
+    const newTask = { ...task, id };
+    setTask((prevState) => [...prevState, newTask]);
+  };
+
+  // REMOVE TASK
+
+  const deleteTask = (id) => {
+    setTask(tasks.filter((task) => id !== task.id));
+  };
+
+  // TOGGLE TASK
+
+  const toggleTask = (id) => {
+    setTask(
+      tasks.map((task) =>
+        task.id === id ? { ...task, isDone: !task.isDone } : { ...task }
+      )
+    );
+  };
+
+  // SHOW FORM
+
+  const showTask = () => setIsShow(!isShow);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center mt-5">
+      <Header showTask={showTask} isShow={isShow} />
+
+      {isShow ? <CreateTask addTask={addTask} /> : ""}
+
+      {tasks.length > 0 ? (
+        <TaskList
+          tasks={tasks}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+        />
+      ) : (
+        <h1 className="text-light">There is no task to do :)</h1>
+      )}
+      <Footer link = {"https://github.com/hakankaraahmet"} text = {"Hakan Karaahmet"}/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/store/InitialState.js
@@ -0,0 +1,20 @@
+export const InitialState = [
+    {
+      id: 1,
+      text: "Study React Pre-Class Notes",
+      day: "2021-02-06",
+      isDone: false,
+    },
+    {
+      id: 2,
+      text: "Feed the Dog",
+      day: "2021-02-05",
+      isDone: false,
+    },
+    {
+      id: 3,
+      text: "Attend in-Class",
+      day: "2021-02-03",
+      isDone: false,
+    },
+];
